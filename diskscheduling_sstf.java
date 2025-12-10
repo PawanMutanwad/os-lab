cb2afb5ecb5f4a0a0bca2393545c95a7228ac281
yuvrajshrirame <mail.yuvrajshrirame@gmail.com>
2025-12-10 17:06:38 +0530

    disk-scheduling: sstf

---
+++ diskscheduling_sstf.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class diskscheduling_sstf {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of disk requests: ");
+        int n = sc.nextInt();
+
+        int[] req = new int[n];   // request sequence
+        boolean[] visited = new boolean[n]; // to mark served requests
+
+        System.out.print("Enter current head position: ");
+        int head = sc.nextInt();
+
+        System.out.println("Enter the request sequence:");
+        for (int i = 0; i < n; i++) {
+            req[i] = sc.nextInt();
+        }
+
+        int totalMovement = 0;
+
+        System.out.println("\nOrder of service:");
+        System.out.print(head);
+
+        // SSTF loop – pick closest request each time
+        for (int count = 0; count < n; count++) {
+
+            int idx = -1;
+            int minDist = Integer.MAX_VALUE;
+
+            // find the nearest unvisited request
+            for (int i = 0; i < n; i++) {
+                if (!visited[i]) {
+                    int distance = Math.abs(req[i] - head);
+
+                    if (distance < minDist) {
+                        minDist = distance;
+                        idx = i;
+                    }
+                }
+            }
+
+            // service the closest request
+            visited[idx] = true;
+            totalMovement += minDist;
+            head = req[idx];
+
+            System.out.print(" -> " + head);
+        }
+
+        System.out.println("\n\nTotal Head Movement = " + totalMovement + " cylinders");
+
+        sc.close();
+    
+    }
+}
